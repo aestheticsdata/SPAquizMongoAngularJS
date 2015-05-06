bc6508ac0598bbc676dde09791b82bfb6fb8fa43
scabel <aesthetics.data@gmail.com>
2015-05-06 17:17:00 +0200

    404

--- build/build.js
+++ build/build.js
@@ -24,13 +24,23 @@ function LoginService() {
 angular.module('SPAquiz', ['SPAquiz.controllers', 'SPAquiz.services', 'ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise('/');
 
         $stateProvider
 
             .state('login', {
-                url:         '/login',
-                templateUrl: 'partials/login.html',
-                controller:  'LoginController'
+                url         : '/login',
+                templateUrl : 'partials/login.html',
+                controller  : 'LoginController'
             })
+
+            .state('404', {
+                url         : '/404',
+                templateUrl : 'partials/404.html'
+            });
+
+        //$urlRouterProvider.otherwise('/404');
+    })
+
+    .run(function ($state) {
+        $state.go('login');
     });
--- js/app.js
+++ js/app.js
@@ -3,13 +3,23 @@
 angular.module('SPAquiz', ['SPAquiz.controllers', 'SPAquiz.services', 'ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise('/');
 
         $stateProvider
 
             .state('login', {
-                url:         '/login',
-                templateUrl: 'partials/login.html',
-                controller:  'LoginController'
+                url         : '/login',
+                templateUrl : 'partials/login.html',
+                controller  : 'LoginController'
             })
+
+            .state('404', {
+                url         : '/404',
+                templateUrl : 'partials/404.html'
+            });
+
+        //$urlRouterProvider.otherwise('/404');
+    })
+
+    .run(function ($state) {
+        $state.go('login');
     });
